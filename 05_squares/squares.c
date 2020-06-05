e6a3d9fb3247bfcd968eedb3b551938a780f23b4
Coursera Learner <nobody@nowhere.nul>
2020-06-05 18:02:14 +0000

    Asingment 5

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -8,8 +8,12 @@
  */
 int isInRange(int coord, int offset, int size) {
   // if coord is in range, return 1
+
+  if (coord>=offset && coord<offset+size ) {
+    return 1; }
+  else {
   // else, return 0
-  return 0;
+    return 0;}
 }
 
 /*
@@ -19,18 +23,49 @@ int isInRange(int coord, int offset, int size) {
 int isAtBorder(int coord, int offset, int size) {
   // if coord is equal to offest or offset + size
   // return 1, else return 0
-  return 0;
+
+  if(coord==offset || coord==offset + size){
+    return 1;}
+  else {
+    return 0;}
 }
+
+int max(int x, int y){
+  if(x>=y){
+    return x;}
+  else {
+    return y;}
+  
+}
+
+int isBetween (int coord ,int x,int y){
+  if (coord>=x && coord<y) {
+    return 1;
+}
+  else {
+    return 0;
+  }}
+
+int isEither(int coord,int x,int y) {
+  if( coord==x||coord==y ){
+    return 1;}
+  else{ return 0;
+  }
+}
+      
+
+
+
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-
+  int w=max(size1,x_offset+size2);
   //compute the max of size1 and (y_offset + size2).  Call this h
-
+  int h=max(size1,y_offset+size2);
   //count from 0 to h. Call the number you count with y
-
+  for(int y=0;y<h;y++) {
     //count from 0 to w. Call the number you count with x
-
+    for(int x=0;x<w;x++) {
       //check if  EITHER
       //    ((x is between x_offset  and x_offset +size2) AND 
       //     y is equal to either y_offset OR y_offset + size2 - 1 )
@@ -38,16 +73,19 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
-
+      if((isBetween(x,x_offset,x_offset+size2)&& isEither(y,y_offset,y_offset+size2-1) )||(isBetween(y,y_offset,y_offset+size2)&& isEither(x,x_offset,x_offset+size2-1))) {printf("*");}
       //if not,
       // check if EITHER
       //    x is less than size1 AND (y is either 0 or size1-1)
       // OR
       //    y is less than size1 AND (x is either 0 or size1-1)
       //if so, print a #
-
+      else if((x<size1 && isEither(y,0,size1-1  ))||(y<size1 && isEither(x,0,size1-1) )) {printf("#");}
       //else print a space
-    //when you finish counting x from 0 to w, 
+      else {printf(" ");}
+    //when you finish counting x from 0 to w,
+    }
+    printf("\n");
     //print a newline
-
+  }
 }
