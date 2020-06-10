2c2671a2111ac4f190b770e0ad0a9c64a1d4bd32
Coursera Learner <nobody@nowhere.nul>
2020-06-10 19:53:39 +0000

    C2W4 final assingment

--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,29 @@
 #include "cards.h"
 
-int main(void) {
 
+
+int main(void) {
+  
+  card_t c;
+  c.value=VALUE_JACK;
+  c.suit=SPADES;
+
+  hand_ranking_t hand=PAIR;
+  
+  
+  assert_card_valid(c);
+  ranking_to_string(hand);
+  print_card(c);
+  card_t d;
+  
+  d=card_from_letters('0','d');
+  print_card(d);
+
+  int num=51;
+  card_t e;
+
+  e=card_from_num(num);
+  print_card(e);
+  
+  return 0;
 }
