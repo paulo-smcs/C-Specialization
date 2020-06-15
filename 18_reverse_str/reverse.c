6c7ff0f5fb6193943c7abddafa544cb66335f56d
Coursera Learner <nobody@nowhere.nul>
2020-06-15 22:31:23 +0000

    Assingment 18

--- 18_reverse_str/reverse.c
+++ 18_reverse_str/reverse.c
@@ -9,6 +9,7 @@ void reverse(char * str) {
 
   if(*test==NULL) {return;}
 
+  
   size_t len=strlen(str);
   char fc;
   char lc;
@@ -34,20 +35,17 @@ void reverse(char * str) {
 }
 
 int main(void) {
-  
-  char str0[]="a" ;
+  char str0[] = "";
   char str1[] = "123";
   char str2[] = "abcd";
-  
-  char * array[] = {str0, str1, str2};
-  for (int i = 0; i <3; i++) {
+  char str3[] = "Captain's log, Stardate 42523.7";
+  char str4[] = "Hello, my name is Inigo Montoya.";
+  char str5[] = "You can be my wingman anyday!";
+  char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
+  char * array[] = {str0, str1, str2, str3, str4, str5, str6};
+  for (int i = 0; i < 7; i++) {
     reverse(array[i]);
     printf("%s\n", array[i]);
   }
-
-  reverse(NULL);
-  reverse(0);
-    
   return EXIT_SUCCESS;
 }
-
